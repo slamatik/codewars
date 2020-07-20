8966e0ba30be8858c1eedb1fd07ca0b9536a51b4
slamatik <vjaceslavsmatess@gmail.com>
2020-07-20 11:34:42 +0100

    Solution

---
+++ 6 kyu/Who likes it 6 kyu.py
@@ -0,0 +1,18 @@
+def likes(names):
+    if len(names) == 0:
+        return "no one likes this"
+    if len(names) == 1:
+        return f"{names[0]} likes this"
+    if len(names) == 2:
+        return f"{names[0]} and {names[1]} like this"
+    if len(names) == 3:
+        return f"{names[0]}, {names[1]} and {names[2]} like this"
+    if len(names) >= 4:
+        return f"{names[0]}, {names[1]} and {len(names[2:])} others like this"
+
+
+print(likes([]))
+print(likes(['Peter']))
+print(likes(['Jacob', 'Alex']))
+print(likes(['Max', 'John', 'Mark']))
+print(likes(['Alex', 'Jacob', 'Mark', 'Max']))
