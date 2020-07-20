dba3ca9c8eddc52e1967437f56abcf0dd220ecf4
slamatik <vjaceslavsmatess@gmail.com>
2020-07-20 11:26:01 +0100

    Solution

---
+++ 6 kyu/Stop gninnipS My sdroW! 6 kyu.py
@@ -0,0 +1,10 @@
+def spin_words(sentence):
+    sentence = sentence.split()
+    for i in range(len(sentence)):
+        if len(sentence[i]) >= 5:
+            sentence[i] = sentence[i][::-1]
+    return " ".join(sentence)
+
+
+print(spin_words("Welcome"))
+print(spin_words("Hey fellow warriors"))
