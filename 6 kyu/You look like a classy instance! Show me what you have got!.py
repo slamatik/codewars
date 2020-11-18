54e58e7eb118728fe9ed22ab82ca6a071bbd9338
slamatik <vjaceslavsmatess@gmail.com>
2020-11-18 11:52:29 +0000

    Solution

---
+++ 6 kyu/You look like a classy instance! Show me what you have got!.py
@@ -0,0 +1,8 @@
+def show_me(instname):
+    attributes = sorted(list(vars(instname).keys()))
+    class_name = type(instname).__name__
+    solution = f"Hi, I'm one of those {class_name}s! Have a look at my "
+    if len(attributes) != 1:
+        return solution + f"{', '.join([attributes[i] for i in range(len(attributes) - 1)])} and {attributes[-1]}"
+    else:
+        return solution + "{attributes[0]}"
