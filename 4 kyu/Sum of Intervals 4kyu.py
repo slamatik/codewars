c85c0969db25d31c9e26aa002d354ab40eabb428
slamatik <vjaceslavsmatess@gmail.com>
2021-01-26 18:41:23 +0000

    Solution

---
+++ 4 kyu/Sum of Intervals 4kyu.py
@@ -0,0 +1,8 @@
+def sum_of_intervals(intervals):
+    return len(set([n for i in intervals for n in range(i[0], i[1])]))
+
+
+print(sum_of_intervals([(1, 5)]))
+print(sum_of_intervals([(1, 5), (6, 10)]))
+print(sum_of_intervals([(1, 5), (1, 5)]))
+print(sum_of_intervals([(1, 4), (7, 10), (3, 5)]))
